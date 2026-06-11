8f959e259de47156925f3ca75aee847dae7e9f63
sagetech24 <sagetechnology.ai@gmail.com>
2026-06-11 10:16:26 +0800

    style:update the page template alignment

--- page.php
+++ page.php
@@ -8,7 +8,7 @@ get_header();
 while (have_posts()) {
     the_post();
     ?>
-    <main class="w-full min-h-[25vh] py-12 lg:py-24">
+    <main class="w-full min-h-[25vh] py-12 lg:py-[6.2rem]">
       <article <?php post_class('remotiq-page-content w-full'); ?>>
         <?php the_content(); ?>
       </article>
